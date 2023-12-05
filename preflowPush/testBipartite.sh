f036867d6118aef188741936f0b75bf28be88f1b
Yexuan Gao <yexuan.gao@outlook.com>
2023-12-04 19:46:15 -0800

    bipartite test

---
+++ preflowPush/testBipartite.sh
@@ -0,0 +1,4 @@
+python3 preflowPush.py "../graphCodePy_Leo/tests/bi-10-10-0.9-1-20.txt"
+python3 preflowPush.py "../graphCodePy_Leo/tests/bi-50-50-0.9-1-100.txt"
+python3 preflowPush.py "../graphCodePy_Leo/tests/bi-100-100-0.9-1-200.txt"
+python3 preflowPush.py "../graphCodePy_Leo/tests/bi-1000-1000-0.9-1-2000.txt"
